8c5a6e9107cf57bcc440c4e328db3a8539e25801
Furkan <frkncesur@gmail.com>
2024-08-26 23:55:47 +0300

    Initial Commit

---
+++ SimpleTimer/SimpleTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTimerApp.swift
+//  SimpleTimer
+//
+//  Created by Furkan Cesur on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
